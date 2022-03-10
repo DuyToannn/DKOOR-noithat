0abfee9d60b1521433947b815d751bcab65be136
Duy Toan <toanpmo123@gmail.com>
2022-03-10 17:11:24 +0700

    up

---
+++ assets/main/main.js
@@ -0,0 +1,6 @@
+// ============ GSAP ============= //
+gsap.from('.animated-h3' , {opacity: 0, duration: 2, delay:.5, x:60,repeatRefresh: true})
+gsap.from('.animated-h1',{opacity: 0, duration: 2, delay:.5, x:60})
+gsap.from('.animated-p',{opacity: 0, duration: 2, delay:.5, x:60})
+gsap.from('.slider-btn',{opacity: 0, duration: 2, delay:.5, x:60})
+gsap.from('.slider-img',{opacity: 0, duration: 2, delay:.5, x:60})
